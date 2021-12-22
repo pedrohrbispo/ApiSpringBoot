7cab2589de29a10982694cda43331560be78f9b9
Pedro Henrique <pedro@lifelink.com.br>
2021-12-22 16:23:57 -0300

    chore: Add spring config files

---
+++ src/main/java/com/pedro/api/MeuProjetoDeEstudosApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoDeEstudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoDeEstudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/api/MeuProjetoDeEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoDeEstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
